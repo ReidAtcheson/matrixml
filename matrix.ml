b78deb2a195ebb8e50b2889381151730a6059d6c
Reid Atcheson <reid.atcheson@gmail.com>
2017-01-05 19:25:48 -0600

    more

--- matrix.ml
+++ matrix.ml
@@ -1,5 +1,37 @@
 
 
 module type Matrix = sig
-  type t
+  type num
+  type mat
+  type slice = { first : int; increment : int; last : int; }
+
+  (*Making matrices*)
+  val make : int*int -> (int->int->num) -> mat
+  val copy : mat -> mat
+
+  (*Destructive update*)
+  val set : mat -> mat -> unit
+
+
+
+  (*Basic matrix properties*)
+  val size : mat -> int*int
+
+
+  (*Basic matrix indexing*)
+  val idget : mat -> int -> int -> num
+  val idset : mat -> int -> int -> num -> unit
+  (*Slice indexing*)
+  val sliceget : mat -> slice -> mat
+
+
+
+  (*Add two matrices*)
+  val add : mat -> mat -> mat  
+  (*Matrix multiplication*)
+  val mul : mat -> mat -> mat
+  (*Scalar multiplication*)
+  val smul : num -> mat -> mat
+  (*Matrix slicing*)
+  val slice : mat -> int -> int -> int -> mat
 end;;
